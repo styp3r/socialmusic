3fdfe5c8a1eec5b3e71c7192549c5f4339fee971
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-08 07:05:19 +0530

    Creates a new component called SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SearchBar(){
+    return (
+    <div id = "searchBar">
+        <div className = "searchInput"></div>
+    </div>);
+}
+
+export default SearchBar;
