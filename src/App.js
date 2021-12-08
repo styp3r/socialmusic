5aa5d7ea049b574f9b00e40e706fd86a776e0c81
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-08 07:03:44 +0530

    Adds SearchBar component to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
+    <SearchBar />
     </div>
   );
 }
