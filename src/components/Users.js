eeea02b10fac3f7acfd8eea43138ea72911e7248
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 02:36:36 +0530

    Changes name from Navi to User

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,43 +1,43 @@
 const users = [
     {
         id: 0,
-        name: "Navi"
+        name: "User"
     },
     {
         id: 1,
-        name: "Navi1"
+        name: "User1"
     },
     {
         id: 2,
-        name: "Navi2"
+        name: "User2"
     },
     {
         id: 3,
-        name: "Navi3"
+        name: "User3"
     },
     {
         id: 4,
-        name: "Navi4"
+        name: "User4"
     },
     {
         id: 5,
-        name: "Navi5"
+        name: "User5"
     },
     {
         id: 6,
-        name: "Navi6"
+        name: "User6"
     },
     {
         id: 7,
-        name: "Navi7"
+        name: "User7"
     },
     {
         id: 8,
-        name: "Navi8"
+        name: "User8"
     },
     {
         id: 9,
-        name: "Navi9"
+        name: "User9"
     },
 ];
 
