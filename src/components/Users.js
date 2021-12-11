35ca691ab320ae894e05726d56625a2e2058578e
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 00:29:45 +0530

    Creates an array of dummy users for testing.

---
+++ src/components/Users.js
@@ -0,0 +1,44 @@
+const users = [
+    {
+        id: 0,
+        name: "Navi"
+    },
+    {
+        id: 1,
+        name: "Navi1"
+    },
+    {
+        id: 2,
+        name: "Navi2"
+    },
+    {
+        id: 3,
+        name: "Navi3"
+    },
+    {
+        id: 4,
+        name: "Navi4"
+    },
+    {
+        id: 5,
+        name: "Navi5"
+    },
+    {
+        id: 6,
+        name: "Navi6"
+    },
+    {
+        id: 7,
+        name: "Navi7"
+    },
+    {
+        id: 8,
+        name: "Navi8"
+    },
+    {
+        id: 9,
+        name: "Navi9"
+    },
+];
+
+export default users;
