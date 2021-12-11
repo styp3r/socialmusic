77ba3f28a511966a5d213772268c1c4988d9a92e
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-11 22:36:40 +0530

    Creates a list of items to display on main feed

---
+++ src/components/TopList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TopListItem from './TopListItem';
+
+function TopList(){
+    return(<div id = "topListContainer">
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+        <TopListItem />
+    </div>);
+}
+
+export default TopList;
