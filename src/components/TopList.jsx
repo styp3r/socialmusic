c15c2570fefd107aa4e5790c84fa4c66a30cd561
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 00:26:34 +0530

    Map through the Users array and display all users

--- src/components/TopList.jsx
+++ src/components/TopList.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import TopListItem from './TopListItem';
+import Users from './Users';
 
 function TopList(){
+    function displayList(d){
+        return <TopListItem key = {d.id} id = {d.id} username = {d.name}/>;
+    }
+
     return(<div id = "topListContainer">
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
-        <TopListItem />
+    {Users.map(displayList)}
     </div>);
 }
 
