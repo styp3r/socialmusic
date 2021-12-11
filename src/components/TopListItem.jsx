0c0dce6e1dd04cfc7631f1315f60792f0a94f560
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 00:28:10 +0530

    Returns user details from array and displays it. Click handler included but has to open with user details in new page - Incomplete.

--- src/components/TopListItem.jsx
+++ src/components/TopListItem.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function TopListItem(){
-    return (<div className = "topListItem"></div>);
+function TopListItem(props){
+
+    let unique = props.id;
+
+    function handleClick(){
+        //open new page with the corresponding user's details
+        document.getElementById(unique).style.backgroundColor = "red";
+    }
+
+    return (
+    <div id = {props.id} className = "topListItem" onClick = {handleClick}>
+        <div className = "profilePicture"></div>
+        <p className = "username">{props.username}</p>
+    </div>);
 }
 
 export default TopListItem;
