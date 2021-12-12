77fdfebdc55d48fd44245152eec33daae4055c02
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 08:14:31 +0530

    Code refactor

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function SearchBar(){
+
     return (
     <div id = "searchBar">
         <i id = "searchIcon" class="fas fa-search"></i>
-        <input className = "searchInput" />
+        <input id = "searchInput" placeholder = "Search"/>
     </div>);
 }
 
