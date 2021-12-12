1b17affacde914cada2af57582efbd886468a6ba
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-13 04:43:16 +0530

    Consists of dummy notifications

---
+++ src/components/Notifications.js
@@ -0,0 +1,40 @@
+const notifs = [
+    {
+        id: "n1",
+        content: "yooooo"
+    },
+    {
+        id: "n1",
+        content: "notif 1"
+    },
+    {
+        id: "n2",
+        content: "notif 2"
+    },
+    {
+        id: "n3",
+        content: "notif 3"
+    },
+    {
+        id: "n4",
+        content: "notif 4"
+    },
+    {
+        id: "n5",
+        content: "notif 5"
+    },
+    {
+        id: "n6",
+        content: "notif 6"
+    },
+    {
+        id: "n7",
+        content: "notif 7"
+    },
+    {
+        id: "n8",
+        content: "notif 8"
+    }
+];
+
+export default notifs;
