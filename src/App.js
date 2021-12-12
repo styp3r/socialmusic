d06b62f22daf4d6ab3855417b0079772656607b8
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 08:13:00 +0530

    Adds popular and goLive components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import TopList from './components/TopList';
 import Navbar from './components/Navbar';
+import NotifTray from './components/NotificationTray';
+import Popular from './components/Popular';
+import GoLive from './components/GoLive';
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
-    <TopList /> 
+    <NotifTray />
+    <div id = "content">
+      <Navbar />
+      <Popular />
+      <GoLive />
+      <TopList />
+    </div> 
     </div>
   );
 }
 
 export default App;
+
