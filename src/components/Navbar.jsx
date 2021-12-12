93a10109b3909c94e4eef37f96308dd9f801a651
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-12 08:13:57 +0530

    Adds logic for handling notifTray click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 function Navbar(){
+
+    function handleClick(){
+        document.getElementById("notifTray").className = "notifTrayAppear";
+        document.getElementById("content").className = "contentBlur";
+    }
+
+
     return(
         <div id = "navbarContainer">
             <SearchBar />
-            <i id = "bellIcon" class="far fa-bell"></i>
+            <i id = "bellIcon" class="far fa-bell" onClick = {handleClick}></i>
             <div id = "myProfileIcon"></div>
         </div>
     );
