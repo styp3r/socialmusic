a66935875e50389d4ab792e14e7f2897429bde0b
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-14 09:45:55 +0530

    Changes placeholder text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ function SearchBar(){
     return (
     <div id = "searchBar">
         <i id = "searchIcon" className="fas fa-search"></i>
-        <input id = "searchInput" placeholder = "Search"/>
+        <input id = "searchInput" placeholder = "Search for people or music"/>
     </div>);
 }
 
